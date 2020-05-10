0c994919c0e6f630422ddc19f5c5b554e6a1a212
looookasz <62953301+looookasz@users.noreply.github.com>
2020-05-10 22:12:19 +0200

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,34 +1,4 @@
-Skip to content
-Search or jump to…
 
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@looookasz 
-Learn Git and GitHub without any code!
-Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
-
-
-vuetifyjs
-/
-vuetify
-84.9k
-575
-25.2k4.2k
- Code
- Issues 1,051
- Pull requests 69
- Discussions Actions
- Security 0
- Insights
-vuetify/packages/docs/src/layouts/layouts/demos/baseline.vue
-@MajesticPotatoe MajesticPotatoe docs(PreMadeLayouts): move link to pen out of example
-a8cc74a 3 days ago
-@johnleider@MajesticPotatoe
-83 lines (79 sloc)  1.86 KB
-  
 <template>
   <v-app id="inspire">
     <v-navigation-drawer
