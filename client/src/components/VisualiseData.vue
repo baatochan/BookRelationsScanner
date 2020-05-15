77b2f5dc4b12cfc93d2a2a067162a393c148283d
DawidPiechota <dpiechotaa@gmail.com>
2020-05-16 00:03:23 +0200

    Add merge function. Hardcoded nodes

--- client/src/components/VisualiseData.vue
+++ client/src/components/VisualiseData.vue
@@ -5,6 +5,7 @@
         <v-text-field v-model="inputUrl" label="Link do testowania"></v-text-field>
         <div class="my-2">
           <v-btn @click="changeData()" small color="primary">Get data</v-btn>
+          <v-btn @click="mergeNodes()" small color="primary">Merge</v-btn>
         </div>
       </div>
     </div>
@@ -21,30 +22,63 @@ export default {
     },
     data() {
       return {
+        originalData: null,
         data: null,
+        changedData: null,
         inputUrl: "https://gist.githubusercontent.com/DawidPiechota/bc9eae88413e3546e7af2a92539f30bc/raw/7a2633381f014dd12feb810e56e6702ab30914a2/data3.json",
         dataList: ['data1.json', 'data2.json', 'data3.json'],
       }
     },
     mounted() {
       this.changeData();
+      //this.changedData = JSON.parse(JSON.stringify(this.originalData)); //Hacky way to copy json
+      //this.data = JSON.parse(JSON.stringify(this.originalData));
     },
     methods: {
+      mergeNodes(){
+        this.markNode("Mariusz");
+        this.mergeLinks(12, 0); //second one stays
+        this.data = JSON.parse(JSON.stringify(this.changedData)); //update the data
+      },
+      mergeLinks(indexA, indexB){
+        var dupa = 0;
+        for (var i = 0; i < this.changedData.links.length; i++) {
+            if (this.changedData.links[i].source == indexA)
+              dupa += 1;
+            if (this.changedData.links[i].target == indexA)
+              dupa += 2;
+
+            switch(dupa) {
+            case 1:
+              this.changedData.links[i].source = indexB
+              break;
+            case 2:
+              this.changedData.links[i].target = indexB
+              break;
+            case 3:
+              delete this.changedData.links[i]
+              break;
+            default:
+              // not possible (i think)
+          }
+        }
+      },
+      markNode(name) {
+        for (var i = 0; i < this.changedData.nodes.length; i++) {
+          if (this.changedData.nodes[i].name == name) {
+            this.changedData.nodes[i].name = "dupa123";
+            return i; //returns node index
+          }
+        }
+      },
       changeData() {
-    //     const dataIndex = Math.floor(Math.random() * this.dataList.length)
-    //     d3.json("https://gist.githubusercontent.com/DawidPiechota/f4e95b3d5d4539c453003136d8f25db9/raw/1f3df31a88a19e20464f9c6b9b12f6ca17e9201d/data2.json",
-    //     function(jsonData) {
-    //     this.data = data;
-    // });
         //wrapper for fetch basically
         d3.json(this.inputUrl)
-        .then(data => {this.data = data})
-
-        // d3.json(this.dataList[dataIndex]).then(data => {
-        //   this.data = data
-        // }).catch(error => {
-        //   console.error('Cannot proceed with simulation, failed to  retrieve data.')
-        // })
+        .then(data => {
+          this.originalData = data
+          this.changedData = JSON.parse(JSON.stringify(data)); //Hacky way to copy json
+          this.data = JSON.parse(JSON.stringify(data));
+          })
       }
     }
   }
