65f8b868dbfe459fd5d143336fe224fc2c0060f6
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2020-05-19 19:14:44 +0200

    Fix formatting of scripts dir

--- scripts/basic.py
+++ scripts/basic.py
@@ -4,23 +4,32 @@ import xml.etree.ElementTree as ET
 import optparse
 
 parser = optparse.OptionParser()
-parser.add_option('-x', '--xml', action="store_true", dest="xml", default=False)
-parser.add_option('-t', '--token-text', action="store_true", dest="token_text", default=False)
-parser.add_option('-b', '--token-bases', action="store_true", dest="token_bases", default=False)
-parser.add_option('-s', '--token-speech', action="store_true", dest="token_speech", default=False)
+parser.add_option('-x', '--xml', action="store_true",
+                  dest="xml", default=False)
+parser.add_option('-t', '--token-text', action="store_true",
+                  dest="token_text", default=False)
+parser.add_option('-b', '--token-bases', action="store_true",
+                  dest="token_bases", default=False)
+parser.add_option('-s', '--token-speech', action="store_true",
+                  dest="token_speech", default=False)
 options, args = parser.parse_args()
 
+
 def ccl_orths(ccl):
-	tree = ET.fromstring(ccl)
-	return [orth.text for orth in tree.iter('orth')]
+    tree = ET.fromstring(ccl)
+    return [orth.text for orth in tree.iter('orth')]
+
 
 def ccl_bases(ccl):
-	tree = ET.fromstring(ccl)
-	return [tok.find('./lex/base').text for tok in tree.iter('tok')]
+    tree = ET.fromstring(ccl)
+    return [tok.find('./lex/base').text for tok in tree.iter('tok')]
+
 
 def ccl_poses(ccl):
-	tree = ET.fromstring(ccl)
-	return [tok.find('./lex/ctag').text.split(":")[0] for tok in tree.iter('tok')]
+    tree = ET.fromstring(ccl)
+    return [tok.find('./lex/ctag').text.split(":")[0]
+            for tok in tree.iter('tok')]
+
 
 clarinpl_url = "http://ws.clarin-pl.eu/nlprest2/base"
 user_mail = "testo@.test.pl"
@@ -46,21 +55,19 @@ ccl = r.content.decode('utf-8')
 
 # The simplest use case
 if (options.xml):
-	print(ccl)
+    print(ccl)
 
 # Print a list of token text forms
 if (options.token_text):
-	orths = ccl_orths(ccl)
-	print(orths)
+    orths = ccl_orths(ccl)
+    print(orths)
 
 # Print a list of token bases
 if (options.token_bases):
-	bases = ccl_bases(ccl)
-	print(bases)
+    bases = ccl_bases(ccl)
+    print(bases)
 
 # Print a list of token part of speech tags
 if (options.token_speech):
-	poses = ccl_poses(ccl)
-	print(poses)
-
-
+    poses = ccl_poses(ccl)
+    print(poses)
