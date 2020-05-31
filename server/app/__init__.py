3d43e842d3c821a724ba553e64446dcd22526b24
Isild <pimok96@gmail.com>
2020-05-31 15:15:51 +0200

    route example working with project structure

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .controllers import controllers as controllers_blueprint
+from .services import services as services_blueprint
 
 app = Flask(__name__)
 db = SQLAlchemy()
+
+app.register_blueprint(services_blueprint)
+app.register_blueprint(controllers_blueprint)
---
+++ server/app/services/services.py
@@ -0,0 +1,306 @@
+import json
+import requests
+import xml.etree.ElementTree as ET
+
+clarinpl_url = "http://ws.clarin-pl.eu/nlprest2/base"
+url = clarinpl_url + "/process"
+user_mail = "testo@.test.pl"
+# Tag and recognize named entities (coarse-grained categories)
+lpmn = 'wcrft2|liner2({"model":"top9"})'
+
+text = "Paweł robi zadanie z Przemek.\
+Przemek współpracuje z Pawłem.\
+Wojtek pisze jutro Kolokwium z angielskiego.\
+Przemek pisze kolokwium z Wojtkiem.\
+Bartosz zrobił już coś.\
+Np. Bartosz zna się tylko z Pawłem.\
+Reszta grupy jest nieznana.\
+Mariusz jedzie autem Mariuszem."
+
+
+def main():
+    # Get analyzed XML
+    info = getTextInf(text)
+
+    dependendencyTable = []
+    index = 0
+    weight = 2**index
+
+    # Entity matrix
+    table = tableInit(info[0], info[1], info[2], info[3], weight)
+    dependendencyTable = table[0]
+    personsTable = table[1]
+
+    for i in dependendencyTable:
+        print(i)
+
+    # -1 because of empty string at the end of text
+    sentencesAmount = len(text.split('.')) - 1
+    # windowSize = 20
+
+    # Get entities relation
+    # div2(info, dependendencyTable, personsTable, sentencesAmount, windowSize)
+    floating_window(info, dependendencyTable, personsTable, sentencesAmount)
+
+    print(personsTable)
+    i = 0
+    for r in dependendencyTable:
+        print(r)
+        i += 1
+
+    print("Summary:")
+    parseData(dependendencyTable, personsTable)
+
+
+def ccl_orths(ccl):
+    tree = ET.fromstring(ccl)
+    return [orth.text for orth in tree.iter('orth')]
+
+
+def ccl_bases(ccl):
+    tree = ET.fromstring(ccl)
+    return [tok.find('./lex/base').text for tok in tree.iter('tok')]
+
+
+def ccl_poses(ccl):
+    tree = ET.fromstring(ccl)
+    return [tok.find('./lex/ctag').
+            text.split(":")[0] for tok in tree.iter('tok')]
+
+
+def ccl_ctag(ccl):
+    tree = ET.fromstring(ccl)
+    return [tok.find('./lex/ctag').
+            text.split(":") for tok in tree.iter('tok')]
+
+
+def getTextInf(textToSend):
+    payload = {'text': textToSend, 'lpmn': lpmn, 'user': user_mail}
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+
+    ccl = r.content.decode('utf-8')
+    bases = ccl_bases(ccl)
+    poses = ccl_poses(ccl)
+    ctag_attr = ccl_ctag(ccl)
+
+    return [ccl, bases, poses, ctag_attr]
+
+
+def tableInit(xml, bases, poses, ctag_attr, weight):
+    len_words = len(poses)
+    count = []
+    personsTable = []
+
+    for i in range(0, len_words-1):
+        if poses[i] == 'subst':
+            if ctag_attr[i][3] == 'm1':
+                if str(bases[i]) in personsTable:
+                    pI = personsTable.index(bases[i])
+                    count[pI] = count[pI] + 1
+                else:
+                    personsTable.append(bases[i])
+                    count.append(1)
+
+    len_ent = len(personsTable)
+    for i in range(0, len_ent):
+        print(personsTable[i], ":", count[i])
+
+    global table_result
+    table_result = [[0 for i in range(len_ent)] for j in range(len_ent)]
+
+    for i in range(0, len_ent):        # entity matrix
+        for j in range(0, len_ent):
+            if i == j:
+                table_result[i][j] = 0
+            else:
+                table_result[i][j] += (count[i] * weight)
+                table_result[j][i] += (count[i] * weight)
+    return [table_result, personsTable]
+
+
+def div2(info, dependendencyTable, personsTable,
+         sentencesAmount, initWindowSize):
+    # Utilising global result and info arrays
+    for z in range(0, sentencesAmount):
+        windowSize = int(initWindowSize / (2**z))
+        if (windowSize >= 1):
+            weight = 2**z
+            for i in range(0, sentencesAmount - 1, windowSize):
+                personsFromWindow = findPersonInWindow(info, i, i + windowSize,
+                                                       sentencesAmount)
+                increaseConnections(dependendencyTable, personsTable,
+                                    personsFromWindow, weight)
+        else:
+            break
+
+
+def floating_window(info, dependendencyTable, personsTable, sentencesAmount):
+    windowSize = sentencesAmount
+    windowStep = 1
+    weight = 1
+    weightStep = 2
+    windowFloatStep = 1
+    # We are decreasing the window size
+    # and increasing the relation weight in each step
+    while (windowSize >= 1):
+        # Last possible position of window (due to its size)
+        lastWinPos = sentencesAmount - windowSize
+        for windowStart in range(0, lastWinPos + 1, windowFloatStep):
+            windowEnd = windowStart + windowSize - 1
+            if (windowEnd >= sentencesAmount):
+                windowEnd = sentencesAmount - 1
+            personsFromWindow = findPersonInWindow(info, windowStart,
+                                                   windowEnd, sentencesAmount)
+            increaseConnections(dependendencyTable, personsTable,
+                                personsFromWindow, weight)
+        windowSize -= windowStep
+        weight *= weightStep
+
+
+def findPersonInWindow(info, indexStart, indexStop, max):
+    stop = indexStop
+    start = indexStart
+
+    if indexStop > max:
+        stop = max
+
+    # Empty array for entities cnt
+    bases = info[1]
+    poses = info[2]
+    ctag_attr = info[3]
+    len_words = len(poses)
+    entities = []
+    count = []
+    isInWindow = False
+    dotCount = 1
+
+    for i in range(0, len_words-1):
+        if poses[i] == 'interp' and poses[i-1] != 'brev':
+            dotCount += 1
+
+        if start <= dotCount:
+            isInWindow = True
+        if stop < dotCount:
+            isInWindow = False
+            break
+
+        if isInWindow:
+            if poses[i] == 'subst':
+                if ctag_attr[i][3] == 'm1':
+                    if str(bases[i]) in entities:
+                        pI = entities.index(bases[i])
+                        count[pI] = count[pI] + 1
+
+                    else:
+                        entities.append(bases[i])
+                        count.append(1)
+    return [entities, count]
+
+
+def increaseConnections(dependendencyTable, personsTable,
+                        winPersonsCnt, weight):
+    entities = winPersonsCnt[0]
+    count = winPersonsCnt[1]
+    len_ent = len(entities)
+
+    for i in range(0, len_ent):
+        person = personsTable.index(entities[i])
+        for r in range(0, len_ent):
+            if r != i:
+                personRel = personsTable.index(entities[r])
+                relStr = (count[i] * weight)
+                dependendencyTable[person][personRel] += relStr
+                dependendencyTable[personRel][person] += relStr
+                dependendencyTable[person][person] += count[i]
+
+
+def parseData(dependendencyTable, personsTable):
+    x = ''
+    x += '{'
+    x += '"nodes": ['
+    pLen = len(personsTable)
+
+    for p in range(0, pLen):
+        if p != 0:
+            x += ', '
+        x += '{ "name": "' + personsTable[p] + '", '
+        x += '"class": "' + personClassification(dependendencyTable,
+                                                 personsTable,
+                                                 dependendencyTable[p][p])
+        x += '" }'
+    x += '],'
+    x += ' "links": ['
+
+    lenP = len(personsTable)
+    data = ''
+    z = 0
+
+    for i in range(0, lenP):
+        for j in range(1+z, lenP):
+            data += '{ "source": ' + str(i) + ', "target": ' + str(j) + \
+                    ', "value": ' + str(dependendencyTable[i][j]) + \
+                    ', "type": "' + \
+                    connectionClassification(dependendencyTable, personsTable,
+                                             dependendencyTable[i][j]) + '" }'
+            data += ', '
+        z += 1
+
+    data = data[:-2]
+    x += data
+    x += '] }'
+
+    y = json.loads(x)
+
+    print(y)
+
+
+def personClassification(dependendencyTable, personsTable, cnt):
+    max = maxPersonCnt(dependendencyTable, personsTable)
+
+    if cnt > (max * 2/3):
+        return 'frequent'
+    elif cnt > (max * 1/3):
+        return 'normal'
+    else:
+        return 'rare'
+
+
+def connectionClassification(dependendencyTable, personsTable, weight):
+    max = maxValOfConnection(dependendencyTable, personsTable)
+
+    if weight > (max * 1/2):
+        return 'straight'
+    else:
+        return 'dotted'
+
+
+def maxValOfConnection(dependendencyTable, personsTable):
+    max = 0
+
+    lenP = len(personsTable)
+    z = 0
+
+    for i in range(0, lenP):
+        for j in range(1+z, lenP):
+            if dependendencyTable[i][j] > max:
+                max = dependendencyTable[i][j]
+        z += 1
+
+    return max
+
+
+def maxPersonCnt(dependendencyTable, personsTable):
+    max = 0
+
+    lenP = len(personsTable)
+
+    for i in range(0, lenP):
+        if dependendencyTable[i][i] > max:
+            max = dependendencyTable[i][i]
+
+    return max
+
+
+if __name__ == "__main__":
+    main()
