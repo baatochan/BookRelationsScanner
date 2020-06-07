34e7ae8833bc69169c08b03e027f823e5df08569
Isild <pimok96@gmail.com>
2020-06-08 01:05:53 +0200

    model

---
+++ server/app/models/graph.py
@@ -0,0 +1,11 @@
+from app.services.db import db
+
+
+class graph(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    ready = db.column(db.Boolean)
+    nodesData = db.Column(db.String(1000000000))
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return "<Currency data: {} {} {} {} >".format(self.id, self.name, self.ready, self.nodesData)
